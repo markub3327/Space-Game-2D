e986c4f6e91f7f2218daacd0083431d7032c1e1d
Martin Horvath <marhor3327@gmail.com>
2019-09-04 14:25:46 +0200

    new repo

---
+++ Assets/Scripts/MoonController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MoonController : MonoBehaviour
+{
+    public float speed = 2;
+
+	public Tilemap tilemap;
+
+    public Vector3Int point;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		// Spin the object around the world origin at 20 degrees/second.
+		transform.RotateAround(tilemap.GetCellCenterWorld(point), Vector3.forward, speed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float torque;
+
+    public float speed;
+
+    private Rigidbody2D rb2D;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        rb2D.AddTorque(torque * (-h) * Time.deltaTime, ForceMode2D.Force);
+
+        rb2D.AddRelativeForce(new Vector2((speed * v * Time.deltaTime), 0.0F), ForceMode2D.Impulse);
+    }
+}
