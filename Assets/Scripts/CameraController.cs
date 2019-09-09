dd3e6203a92e601fb1100d38baa20a770e9b7fed
Martin Horvath <marhor3327@gmail.com>
2019-09-09 08:59:23 +0200

    news
    new engines + new player controller + new moons

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+	public Transform playerTransform;
+
+	//public float follow_tightness = 1.0f;
+
+
+	// Use this for initialization
+	void Start()
+    {
+
+	}
+
+
+    // LateUpdate is called after all Update functions have been called
+    void LateUpdate()
+    {
+        // Nastav kamere novu poziciu
+        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, transform.position.z);
+	}	
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float torque = 150.0f;
+
+    public float speed = 20.0f;
+
+    private Rigidbody2D rb2D;
+
+    private ParticleSystem[] pSystems;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        pSystems = GetComponentsInChildren<ParticleSystem>();
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // Pridaj silu do rotacie lode
+        rb2D.AddTorque(torque * (-h) * Time.deltaTime, ForceMode2D.Force);
+
+        // Pridaj silu do pohybu lode
+        rb2D.AddRelativeForce(new Vector2((speed * v * Time.deltaTime), 0.0F), ForceMode2D.Impulse);
+
+        // Zapni animaciu pohonu
+        if (System.Math.Abs(h) > 0.0f || System.Math.Abs(v) > 0.0f)
+        {
+            foreach (var pSystem in pSystems)
+            {
+                if (!pSystem.isEmitting)
+                    pSystem.Play(true);
+            }
+        }
+    }
+}
